cedafaaf0c1ef61089dfcb249e04e26611153999
Kenro Hontoku <kenro961201bptzd@gmail.com>
2026-02-02 14:43:20 +0900

    修正

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,5 +10,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
     appId: "1:861182613182:web:ba7b947993fa3c0610f0ef"
   };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: "AIzaSyBNdUCOex6fVdBBfiMXyWhJtmVFwcf_Tdo",
+    authDomain: "pwa-push-test-2283d.firebaseapp.com",
+    projectId: "pwa-push-test-2283d",
+    storageBucket: "pwa-push-test-2283d.firebasestorage.app",
+    messagingSenderId: "861182613182",
+    appId: "1:861182613182:web:ba7b947993fa3c0610f0ef"
+  });
+
 firebase.messaging();
