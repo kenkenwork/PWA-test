b45d3f20e9b8e43c1397c51580944235d40aa670
Kenro Hontoku <kenro961201bptzd@gmail.com>
2026-02-02 14:47:10 +0900

    バージョン

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyBNdUCOex6fVdBBfiMXyWhJtmVFwcf_Tdo",
